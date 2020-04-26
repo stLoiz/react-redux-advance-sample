27f717379d57dcee07e338f389f5f97b6f84c18d
stella-loizou <loizou.stella@gmail.com>
2020-04-26 15:02:30 +0100

    Add a utility function to make the reducers a little bit more leaner

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   counter: 0,
@@ -6,26 +7,16 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
+      return updateObject(state, { counter: state.counter + 1 });
+
     case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
+      return updateObject(state, { counter: state.counter - 1 });
 
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value,
-      };
+      return updateObject(state, { counter: state.counter + action.value });
+
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value,
-      };
+      return updateObject(state, { counter: state.counter - action.value });
   }
 
   return state;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: [],
@@ -6,17 +7,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.value }),
-      };
+      });
+
     case actionTypes.REMOVE_RESULT:
       const newArray = [...state.results];
       const updatedArray = newArray.filter((result) => result.id !== action.id);
-      return {
-        ...state,
-        results: updatedArray,
-      };
+      return updateObject(state, { results: updatedArray });
   }
 
   return state;
